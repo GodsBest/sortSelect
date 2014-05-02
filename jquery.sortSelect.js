0ccbc1d1032291ce6e5f47fd4f1ccf514164e644
GodsBest <peter.aganyo@gmail.com>
2014-05-02 04:12:43 -0400

    added ability to use .val() or .text() for sorting - [use:'val'|'text']

--- jquery.sortSelect.js
+++ jquery.sortSelect.js
@@ -1,11 +1,11 @@
 ;( function( $ ) {
     $.fn.sortSelect = function( options ) {
-        var def = $.extend({},{order:'asc'},options);
+        var def = $.extend({},{order:'asc',use:'val'},options);
         var ret = { an: { 'asc': -1, 'desc': 1 }, bn: { 'asc': 1, 'desc': -1 } };
         return this.each( function() {
-            var opt = $( this ).find( 'option' ).filter(function() { return this.value != ''; } ).sort( function( a, b ) {
-                an = a.value.toLowerCase();
-                bn = b.value.toLowerCase();
+            var opt = $( this ).find( 'option' ).filter(function() { return $( this )[def.use]() != '' && this.value != ''; } ).sort( function( a, b ) {
+                an = $( a )[def.use]().toLowerCase();
+                bn = $( b )[def.use]().toLowerCase();
                 if( an < bn ) {
                     return ret.an[def.order];
                 } else if( bn < an ) {
@@ -15,7 +15,7 @@
                 }
             })
             .clone();
-            $( this ).find( 'option' ).filter(function() { return this.value != ''; } ).remove();
+            $( this ).find( 'option' ).filter(function() { return $( this )[def.use]() != '' && this.value != ''; } ).remove();
             $( this ).append( opt );
         });
     };
