0983abec151016e6cabcaf1d7dfbf2798226bd50
GodsBest <peter.aganyo@gmail.com>
2017-09-21 15:28:01 -0400

    Relocated curlies

--- jquery.sortSelect.js
+++ jquery.sortSelect.js
@@ -1,48 +1,30 @@
-;( function( $ ) 
-{
-	$.fn.sortSelect = function( options ) 
-	{
+;( function( $ ) {
+	$.fn.sortSelect = function( options ) {
         var def = $.extend( {}, { order: 'asc', use: 'val', ignorecase: true, type: 'alpha' }, options );
 		var ret = { an: { 'asc': -1, 'desc': 1 }, bn: { 'asc': 1, 'desc': -1 } };
-		return this.each( function() 
-		{
+		return this.each( function() {
 			var opt = $( this ).find( 'option' ).filter(function() { return $( this )[def.use]() != '' && this.value != ''; } ).get()
-			.sort( function( a, b ) 
-			{
+			.sort( function( a, b ) {
 				an = $( a )[def.use]();
 				bn = $( b )[def.use]();
-				if( def.type == 'alpha' ) 
-				{
-					!def.ignorecase || (function()
-					{
+				if( def.type == 'alpha' ) {
+					!def.ignorecase || (function() {
 						an = an.toLowerCase();
 						bn = bn.toLowerCase();
 					})();
-					if( an < bn ) 
-					{
+					if( an < bn ) {
 						return ret.an[def.order];
-					} 
-					else if( bn < an ) 
-					{
+					} else if( bn < an ) {
 						return ret.bn[def.order];
-					} 
-					else 
-					{
+					} else {
 						return 0;
 					}
-				} 
-				else if( def.type == 'numeric' ) 
-				{
-					if( def.order == 'asc' ) 
-					{
+				} else if( def.type == 'numeric' ) {
+					if( def.order == 'asc' ) {
 						return an - bn;
-					} 
-					else if( def.order == 'desc' ) 
-					{
+					} else if( def.order == 'desc' ) {
 						return bn - an;
-					} 
-					else 
-					{
+					} else {
 						return 0;
 					}
 				}
